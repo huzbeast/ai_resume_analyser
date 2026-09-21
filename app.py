7cab24d7219d91a382ac7d8423bccf0420effcd3
Huzaifa Modiwala <huz123f5@gmail.com>
2026-09-21 17:32:23 +0530

    built resume text extraction, and intewgreted with app.py

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 import streamlit as st
-
+from resume_parser import extract_resume_text
 st.title("AI Resume Analyzer")
 
-st.write("Resume Analyzer project is working.")
+uploaded_file = st.file_uploader(
+    "Upload your resume",
+    type = ["pdf","docx"]
+)
+
+if uploaded_file is not None:
+    st.write("File uploaded:", uploaded_file.name)
+
+    resume_text = extract_resume_text(uploaded_file)
+
+    st.subheader("Extracted Resume Text")
+
+    st.text_area(
+        "Resume Content",
+        resume_text,
+        height=400
+    )
